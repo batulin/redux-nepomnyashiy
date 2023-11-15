44743d4d89460803b85a370e069ab1485d96ece4
evgen <batulin-1983@mail.ru>
2023-11-15 21:16:50 +0600

    1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+  const [todos, setTodos] = useState();
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
